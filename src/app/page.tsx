94d7fd1f86add47c1e55bf3ef4933238a1224841
Nick Michau <70205169+NOM1989@users.noreply.github.com>
2025-08-01 17:00:19 +0100

    add: image to slide 2 pre anim

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import { ScrollTransformSlide, useScrollFade } from "./components/ScrollTransformSlide";
 
 export default function Home() {
@@ -44,11 +45,27 @@ export default function Home() {
       </section>
 
       {/* Slide 2 - Coming Soon */}
-      <section className="sticky top-0 h-screen flex flex-col items-center justify-center text-center font-inconsolata">
-        <ScrollTransformSlide className="w-full h-full flex flex-col items-center justify-center text-center">
-          <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-8xl text-white glow">
-            Content coming...
-          </h1>
+      <section className="sticky top-0 h-screen flex flex-col items-center justify-center text-center font-inconsolata glow">
+        <ScrollTransformSlide className="w-full h-full flex flex-col items-center justify-center text-center space-y-8">
+          <div className="flex flex-col items-center">
+            {/* Profile Image */}
+            <div className="relative w-48 h-48 md:w-64 md:h-64 lg:w-80 lg:h-80">
+              <Image
+                src="/nick-cutout.png"
+                alt="Nick Michau"
+                fill
+                className="object-contain filter brightness-90 contrast-110"
+                priority
+              />
+            </div>
+            
+            {/* Content Divider */}
+            <div className="w-64 md:w-80 lg:w-96 border-b-2 border-white"></div>
+          </div>
+          
+          <div className="space-y-4">
+            <h1 className="text-4xl">Living and learning on the cutting edge.</h1>
+          </div>
         </ScrollTransformSlide>
       </section>
     </main>
